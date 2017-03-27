927204f3b5662e6474e53c8679ca779e3f995c91
mac <mac.gainor@gmail.com>
2017-03-26 17:10:00 -0700

    removed debugging

--- src/js/components/PrettyJson.js
+++ src/js/components/PrettyJson.js
@@ -4,17 +4,16 @@ import PrettyObject from './PrettyObject';
 require('./../../style/app-globals.scss');
 
 export default class extends React.Component {
-	constructor(props) {
-		super(props)
-		console.log(props)
-	}
+    constructor(props) {
+        super(props)    
+    }
 
-	render = () => {
-		const {src} = this.props;
-		return (
-		<div class="pretty-json-container">
-			<PrettyObject depth={0} >{src}</PrettyObject>
-		</div>
-		);
-	}
+    render = () => {
+        const {src} = this.props;
+        return (
+        <div class="pretty-json-container">
+            <PrettyObject depth={0} >{src}</PrettyObject>
+        </div>
+        );
+    }
 }

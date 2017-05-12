6463086b59a47b2aef2010d88e13fa31139ca5a9
mac <mac.gainor@gmail.com>
2017-05-12 10:20:36 -0700

    more tests

---
+++ test/components/VariableMeta-test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import {expect} from 'chai';
+
+import VariableMeta from './../../src/js/components/VariableMeta';
+
+describe('<VariableMeta />', function () {
+    const rjvId = 1;
+
+
+
+
+});

2ae4e928560f8350db38ce61544fc387f408d6e5
mac <mac.gainor@gmail.com>
2017-06-04 15:14:47 -0700

    added support for dates

---
+++ test/tests/js/components/DataTypes/Date-test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {expect} from 'chai';
+
+import JsonDate from '/react/src/js/components/DataTypes/Date';
+import DataTypeLabel from '/react/src/js/components/DataTypes/DataTypeLabel';
+
+describe('<JsonDate />', function () {
+    const rjvId = 1;
+
+    it('integer component should have a data type label', function () {
+        const wrapper = shallow(
+            <JsonDate
+            variable={{value: new Date()}}
+            displayDataTypes={true}
+            rjvId={rjvId}
+            theme='rjv-default'/>
+        );
+        expect(wrapper.find(DataTypeLabel)).to.have.length(1);
+    });
+
+});

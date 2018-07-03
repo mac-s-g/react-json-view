336ec21c541fa90a4eb31e8102af48986e0d8afe
Caina Leao <caina.leao@endemolshine.com>
2018-07-03 13:29:31 +0100

    Added `defaultValue` prop to be passed down to `AddKeyRequest` which will be then set when a new key is added to json

--- src/js/index.js
+++ src/js/index.js
@@ -56,7 +56,8 @@ class ReactJsonView extends React.PureComponent {
         onSelect: false,
         iconStyle: 'triangle',
         style: {},
-        validationMessage: 'Validation Error'
+        validationMessage: 'Validation Error',
+        defaultValue: null,
     }
 
     // will trigger whenever setState() is called, or parent passes in new props.
@@ -176,7 +177,7 @@ class ReactJsonView extends React.PureComponent {
             name
         } = this.state;
 
-        const { style } = this.props;
+        const { style, defaultValue } = this.props;
 
         return (
             <div
@@ -198,7 +199,8 @@ class ReactJsonView extends React.PureComponent {
                 <AddKeyRequest
                     active={addKeyRequest}
                     theme={theme}
-                    rjvId={this.rjvId} />
+                    rjvId={this.rjvId}
+                    defaultValue={defaultValue} />
             </div>
         );
     }

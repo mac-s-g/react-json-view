cb0f054288858d8dc32adec5aba7f6cf346bde9b
dncdrn <missnentin@gmail.com>
2020-07-18 16:07:00 +0800

    added storeReponsePathData in props added keys for getting object keys of source data from props added class highlight for id equal to storeResponsePath added JSonVariable class for storing object key name from source data

--- src/js/components/ObjectName.js
+++ src/js/components/ObjectName.js
@@ -1,32 +1,72 @@
-import React from 'react';
-import Theme from './../themes/getStyle';
+import React from "react";
+import Theme from "./../themes/getStyle";
+import { toType } from "../helpers/util";
 
 export default function getObjectName(props) {
-    const {
-        parent_type, namespace, theme, jsvRoot, name
-    } = props;
+  const {
+    parent_type,
+    namespace,
+    theme,
+    jsvRoot,
+    name,
+    src,
+    storeResponsePathData,
+  } = props;
 
-    const display_name = props.name ? props.name : '';
+  const dotNotationNamespace = namespace.join(".");
 
-    if (jsvRoot && (name === false || name === null)) {
-        return (<span />);
-    } else if (parent_type == 'array') {
-        return (
-            <span {...Theme(theme, 'array-key')} key={namespace}>
-                <span class="array-key">{display_name}</span>
-                <span {...Theme(theme, 'colon')}>:</span>
-            </span>
-        );
-    } else {
-        return (
-            <span {...Theme(theme, 'object-name')} key={namespace}>
-                <span class="object-key">
-                    <span style={{verticalAlign:'top'}}>"</span>
-                    <span>{display_name}</span>
-                    <span style={{verticalAlign:'top'}}>"</span>
-                </span>
-                <span {...Theme(theme, 'colon')}>:</span>
-            </span>
-        );
+  let variable;
+  let keys = Object.keys(src || {});
+
+  keys.forEach((name) => {
+    variable = new JsonVariable(name, src[name]);
+
+    if (!src.hasOwnProperty(name)) {
+      return;
     }
+  });
+  let highlight;
+  if (storeResponsePathData) {
+    storeResponsePathData.forEach((store) => {
+      if (store.storeResponsePath === dotNotationNamespace) highlight = true;
+    });
+  }
+
+  const display_name = props.name ? props.name : "";
+  if (jsvRoot && (name === false || name === null)) {
+    return <span />;
+  } else if (parent_type == "array") {
+    return (
+      <span {...Theme(theme, "array-key")} key={namespace}>
+        <span
+          class={highlight ? "highlight array-key" : "array-key"}
+          id={dotNotationNamespace}
+        >
+          {display_name}
+        </span>
+        <span {...Theme(theme, "colon")}>:</span>
+      </span>
+    );
+  } else {
+    return (
+      <span {...Theme(theme, "object-name")} key={namespace}>
+        <span class="object-key">
+          <span style={{ verticalAlign: "top" }}>"</span>
+          <span className={highlight && "highlight"} id={dotNotationNamespace}>
+            {display_name}
+          </span>
+          <span style={{ verticalAlign: "top" }}>"</span>
+        </span>
+        <span {...Theme(theme, "colon")}>:</span>
+      </span>
+    );
+  }
+}
+//just store name, value and type with a variable
+class JsonVariable {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+    this.type = toType(value);
+  }
 }

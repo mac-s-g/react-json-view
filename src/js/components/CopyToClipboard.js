80ebfc497b2f7a33463a937a51fb8cb1b721009d
Ming Horn <mhorn@nerdwallet.com>
2020-10-23 13:51:06 -0700

    Use clipboardy instead of execCommand

--- src/js/components/CopyToClipboard.js
+++ src/js/components/CopyToClipboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import clipboardy from 'clipboardy';
 
 import { toType } from './../helpers/util';
-import stringifyVariable from './../helpers/stringifyVariable';
 
 //clibboard icon
 import { Clippy } from './icons';
@@ -27,26 +27,15 @@ export default class extends React.PureComponent {
     }
 
     handleCopy = () => {
-        const container = document.createElement('textarea');
         const { clickCallback, src, namespace } = this.props;
-
-        container.innerHTML = JSON.stringify(
-            this.clipboardValue(src),
-            null,
-            '  '
-        );
-
-        document.body.appendChild(container);
-        container.select();
-        document.execCommand('copy');
-
-        document.body.removeChild(container);
+        clipboardy.writeSync(JSON.stringify(this.clipboardValue(src), null, '  '));
 
         this.copiedTimer = setTimeout(() => {
             this.setState({
                 copied: false
             });
         }, 5500);
+
 
         this.setState({ copied: true }, () => {
             if (typeof clickCallback !== 'function') {

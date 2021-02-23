f4fe4080d25adec5ea656446e3511ef8f99fd757
superfaz <16510828+superfaz@users.noreply.github.com>
2021-02-23 01:05:00 +0100

    Update JsonViewer.js
    Fix code style

--- src/js/components/JsonViewer.js
+++ src/js/components/JsonViewer.js
@@ -8,7 +8,9 @@ export default class extends React.PureComponent {
         const namespace = [props.name];
         let ObjectComponent = JsonObject;
 
-        const size = Array.isArray(props.src) ? props.src.length: Object.keys(props.src).length;
+        const size = Array.isArray(props.src)
+            ? props.src.length
+            : Object.keys(props.src).length;
         if (
             props.groupArraysAfterLength &&
             size > props.groupArraysAfterLength

bb1a43f819b8d0e7836b5f97c7568d51a2ae9df4
Kaspar Arme <kaspar.arme@reach-u.com>
2021-09-29 14:17:25 +0300

    Switch to Draft.js editor

--- src/js/components/MarkdownEditor.js
+++ src/js/components/MarkdownEditor.js
@@ -1,27 +1,71 @@
 import React from 'react';
-import MDEditor from '@uiw/react-md-editor';
-import '@uiw/react-md-editor/markdown-editor.css';
+import { Editor, EditorState, RichUtils, convertFromRaw, convertToRaw } from 'draft-js';
+import { draftToMarkdown, markdownToDraft } from 'markdown-draft-js';
+import 'draft-js/dist/Draft.css';
+
+const styles = [
+    {label: 'B', style: 'BOLD'},
+    {label: 'I', style: 'ITALIC'},
+    {label: 'U', style: 'UNDERLINE'}
+];
 
 export default class extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            value: props.value,
+            value: EditorState.createWithContent(convertFromRaw(markdownToDraft(props.value))),
         }
     }
 
+    componentDidMount() {
+        if (this.editor) {
+            this.editor.focus();
+        }
+    }
+
+    handleStyleToggle = (style) => {
+        this.handleChange(RichUtils.toggleInlineStyle(this.state.value, style));
+    }
+
     handleChange = (value) => {
-        const pattern = new RegExp(/\n/g);
-        const formatted = value.replace(pattern, '  \n');
-        this.setState({ value: formatted });
+        this.setState({ value });
         if (this.props.onChange) {
-            this.props.onChange(value);
+            const plainText = draftToMarkdown(convertToRaw(value.getCurrentContent()));
+            this.props.onChange(plainText);
         }
     }
 
     render() {
         return (
-            <MDEditor value={this.state.value} onChange={this.handleChange} />
+            <div>
+                {styles.map(type => (
+                    <StyleButton
+                        label={type.label}
+                        style={type.style}
+                        onToggle={this.handleStyleToggle}
+                        active={this.state.value.getCurrentInlineStyle().has(type.style)}
+                    />
+                ))}
+                <Editor
+                    ref={el => this.editor = el}
+                    editorState={this.state.value}
+                    onChange={this.handleChange}
+                />
+            </div>
         )
     }
 }
+
+const StyleButton = ({ label, style, onToggle, active }) => {
+    const _onToggle = (e) => {
+        e.preventDefault();
+        onToggle(style);
+    };
+    const styles = {
+        fontWeight: active ? 'bold' : 'normal',
+    };
+
+    return (
+        <span onMouseDown={_onToggle} style={styles}>{label}</span>
+    );
+};

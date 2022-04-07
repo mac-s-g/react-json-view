9e9e9cb0fdeeb73ae63898068be4e52557a0a8b3
Victoria Sokolova <viktoriia.sokolova@dataart.com>
2022-04-07 10:53:36 +0300

    removed unused

--- dev-server/src/index.js
+++ dev-server/src/index.js
@@ -19,7 +19,6 @@ ReactDom.render(
             src={getExampleJson1()}
             quotesOnKeys={false}
             collapseStringsAfterLength={12}
-            nonEditableFields={['integer']}
             onEdit={e => {
                 console.log('edit callback', e);
                 if (e.new_value == 'error') {

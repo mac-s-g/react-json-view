549bdea03aaf395d57a59b5dea61a02c205d015b
Pawel Zielinski <p.zielinski@livesession.io>
2022-06-01 09:47:31 +0200

    documentation

--- doc/example-usage.js
+++ doc/example-usage.js
@@ -1,5 +1,5 @@
 
-import ReactJson from 'react-json-view';
+import ReactJson from 'react-json-view-2';
 
 //you have some json to display
 let my_important_json = {
@@ -31,7 +31,7 @@ let my_important_json = {
 
 
 // ES6 with React
-import ReactJson from 'react-json-view';
+import ReactJson from 'react-json-view-2';
 
 ...
 <ReactJson src={my_important_json} />

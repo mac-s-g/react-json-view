cd70696e2fce0c10828b5c5548acf9f65919b566
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2022-12-15 22:08:38 +0530

    test: added test for data-type-label

---
+++ test/tests/js/components/DataTypes/DataTypeLabel.test.tsx
@@ -0,0 +1,53 @@
+import "@testing-library/jest-dom";
+
+import { render } from "@testing-library/react";
+import { cleanup } from "@testing-library/react-hooks";
+
+import DataTypeLabel from "../../../../../src/js/components/DataTypes/DataTypeLabel";
+import ReactJsonViewContext from "../../../../../src/js/components/ReactJsonViewContext";
+
+describe("<DataTypeLabel />", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("DataTypeLabel should exist when displayDataTypes is true", () => {
+    const rendered = render(
+      <ReactJsonViewContext.Provider
+        value={{
+          /* @ts-ignore */
+          props: {
+            theme: "rjvDefault",
+            displayDataTypes: true,
+          },
+        }}
+      >
+        <DataTypeLabel typeName="string" />
+      </ReactJsonViewContext.Provider>,
+    );
+
+    const dataTypelabelElem =
+      rendered.container.querySelectorAll(".data-type-label");
+    expect(dataTypelabelElem).toHaveLength(1);
+  });
+
+  it("DataTypeLabel should not exist when displayDataTypes is false", () => {
+    const rendered = render(
+      <ReactJsonViewContext.Provider
+        value={{
+          /* @ts-ignore */
+          props: {
+            theme: "rjvDefault",
+            displayDataTypes: false,
+          },
+        }}
+      >
+        <DataTypeLabel typeName="string" />
+      </ReactJsonViewContext.Provider>,
+    );
+
+    const dataTypelabelElem =
+      rendered.container.querySelectorAll(".data-type-label");
+    expect(dataTypelabelElem).toHaveLength(0);
+  });
+});

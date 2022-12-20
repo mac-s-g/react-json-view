da1a119b790568ec63da0c8eb3c85de7e05c0613
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2022-12-20 22:18:49 +0530

    test: added test for get-style

---
+++ test/tests/js/themes/getStyle.test.tsx
@@ -0,0 +1,25 @@
+import getStyle from "../../../../src/js/themes/getStyle";
+
+describe("getStyle", () => {
+  const rjvId = 1;
+
+  it("test that style is returned", () => {
+    const style = getStyle("rjvDefault", "app-container");
+    expect(style.style.cursor).toBe("default");
+  });
+
+  it("test objectKeyVal return", () => {
+    const style = getStyle("rjvDefault", "objectKeyVal", { paddingLeft: 10 });
+    expect(style.style).toBeDefined();
+  });
+
+  it('test "none" theme', () => {
+    const style = getStyle("none", "app-container");
+    expect(style.style).toBeDefined();
+  });
+
+  it("test theme not set (NOTICE OUTPUT EXPECTED)", () => {
+    const style = getStyle("", "app-container");
+    expect(style.style).toBeDefined();
+  });
+});
